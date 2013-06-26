45c240d95944363dc8bd150c81bc0f22c8797f21
Julien Vehent <julien@linuxwall.info>
2013-06-25 22:12:24 -0400

    Make things fast: iterate through the list of file, and perform all checks for each file at once
    For MD5, the sum of the file is calculated and compared with all wanted hashes
    For regexes, all regexes are compiled at startup, then the file is read line by line, and each
    line is tested against all the regexes.
    
    This is easily one or two orders of magnitude faster!

--- filechecker.py
+++ filechecker.py
@@ -1,60 +1,83 @@
 #!/usr/bin/env python
+# ulfr - 2013
 
 import hashlib
 from re import compile, match
+from collections import defaultdict
 from optparse import OptionParser
 from time import sleep
 import os
+from sys import exit
+
+# FIXME: don't pass global dict around, use 'return' instead
+CHECKLIST = {}
+""" CHECKLIST contains a dict of files with associated checks """
+RESULTS = {}
+""" RESULTS contains a dict of IOCs with positive and negative matches """
+counter = 0
+""" Keep trac of things """
+
+def build_dict_of_checks(checks, mode):
+	d = {}
+	for ioc in checks:
+		if checks[ioc]['mode'] == mode:
+			d[ioc] = checks[ioc]['check']
+	return d
 
 
-def file_has_md5(f, h):
+def compute_md5_of_file(f):
 	"""
-		Calculate the MD5 hash of a file, block by block,
-		and return True if it matches the submitted hash
+		Return the MD5 hash of a file, computed block by block,
 	"""
 	BLOCK_SIZE = 128
 	md5 = hashlib.md5()
 	if options.debug:
-		print("[DEBUG] file_has_md5: hash '%s' file '%s'" % (h, f))
+		print("[DEBUG] compute_md5_of_file: called with file '%s'" % (f,))
 	try:
 		fd = open(f)
 	except IOError:
 		if options.verbose:
-			print("[ERROR] file_has_md5: Can't open '%s'" % f)
+			print("[ERROR] file_has_md5: Can't open '%s'" % (f,))
+		return None
 	while True:
 		data = fd.read(BLOCK_SIZE)
 		if not data:
 			break
 		md5.update(data)
 	fd.close()
-	if h == md5.hexdigest():
-		if options.debug:
-			print("[DEBUG] file_has_md5: match found file '%s' hash '%s'" %
-				  (f, h))
-		return True
-	return False
+	return md5.hexdigest()
 
 
-def file_has_regex(f, regex):
+def add_file_to_checklist(f, ioc, mode, check):
 	"""
-		Look for a matching regex inside a file, line by line,
-		and return True if found
+		Add a file to the global checklist
 	"""
-	fd = open(f)
 	if options.debug:
-		print("[DEBUG] file_has_regex: checking file '%s'" % (f,))
-	try:
-		fd = open(f)
-	except IOError:
-		if options.verbose:
-			print("[ERROR] file_has_regex: Can't open '%s'" % f)
-	for line in fd:
-		if regex.match(line):
-			if options.debug:
-				print("[DEBUG] file_has_regex: match found in file '%s'" %
-					  (f,))
-			return True
-	return False
+		print("Adding file '%s' mode '%s' check '%s'" % (f, mode, check))
+	if f not in CHECKLIST:
+		CHECKLIST[f] = {}
+	modes = CHECKLIST[f].get('modes', [])
+	if mode not in modes:
+		modes.append(mode)
+		CHECKLIST[f]['modes'] = modes
+	iocs = CHECKLIST[f].get('iocs', {})
+	if mode == 'regex':
+		""" compile the regex	first, then store it """
+		regex = compile(check)
+		iocs[ioc] = { 'mode': mode, 'check': regex }
+	else:
+		iocs[ioc] = { 'mode': mode, 'check': check }
+	CHECKLIST[f]['iocs'] = iocs
+
+
+def store_match(f, ioc, counter):
+	CHECKLIST[f]['iocs'][ioc]['result'] = True
+	if ioc not in RESULTS:
+		RESULTS[ioc] = []
+	RESULTS[ioc].append(f)
+	if options.debug:
+		print("[DEBUG] IOC '%s' found match on file '%s'" % (ioc, f))
+	return counter + 1
 
 
 usage = """%prog [options] <path>:regex=<regex> <path>:md5=<hash> ...
@@ -76,68 +99,46 @@ parser.add_option("-d", "--debug",
                   help="debug logging, to impress yours friends")
 (options, args) = parser.parse_args()
 
-if options.verbose:
-	print "Starting search. Target list: %s" % args
-
-IOCS = {}
 for ioc in args:
-	""" IOCs are stored in positional arguments
-		the format is <file path>:<mode>=<check value>
+	"""
+		IOCs are stored in positional arguments, format is <path>:<mode>=<check>
+		This loop parses the arguments and build the list of files to check
 	"""
 	path = ioc.split(':')[0]
-	mode = ioc.split(':')[1].split('=')[0]
-	check = ioc.split(':')[1].split('=')[1]
-	IOCS[ioc] = {'path': path, 'mode': mode, 'check': check}
-	if options.verbose:
-		print("Adding IOC '%s' with mode '%s' check '%s'" %
-			  (IOCS[ioc]['path'], mode, check))
-
-for ioc in IOCS:
-	"""
-		Build list of files from the path in the IOC
-	"""
-	path = IOCS[ioc]['path']
-	IOCS[ioc]['files'] = {}
+	(mode, check) = ioc.split(':')[1].split('=')
+	if mode not in ['regex', 'md5', 'sha1']:
+		print("[ERROR] Unknown IOC mode '%s'" % (mode,))
+		sys.exit(1)
 	if os.path.isdir(path):
 		for r,d,flist in os.walk(path):
 			for f in flist:
-				IOCS[ioc]['files'][os.path.join(r,f)] = None
-				if options.debug:
-					print("Adding file '%s' to target list for IOC '%s'" %
-						  (os.path.join(r,f), ioc))
+				f_abs = os.path.join(r,f)
+				add_file_to_checklist(f_abs, ioc, mode, check)
 	elif os.path.isfile(path):
-		IOCS[ioc]['files'][path] = None
+		add_file_to_checklist(path, ioc, mode, check)
 
-for ioc in IOCS:
+for f in CHECKLIST:
 	"""
-		For each file listed in each IOC, perform the check
-		according to the mode
-		'regex' mode executes the regex inside the file
-		'md5' and 'sha1' modes calculate a hash of the file and compare it to
-		the provided hash value
+		Check the files listed in the CHECKLIST dict.
 	"""
-	if options.verbose:
-		print("Checking IOC '%s' against %s files" %
-			  (ioc, len(IOCS[ioc]['files'])))
-	mode = IOCS[ioc]['mode']
-	check = IOCS[ioc]['check']
-	for f in IOCS[ioc]['files']:
-		if options.throttle > 0:
-			sleep(options.throttle)
-		if options.debug:
-			print("[DEBUG] checking '%s' against '%s' in mode '%s'" %
-				  (f, check, mode))
-		if mode == 'regex':
-			regex = compile(check)
-			IOCS[ioc]['files'][f] = file_has_regex(f, regex)
-			continue
-		if mode == 'md5':
-			IOCS[ioc]['files'][f] = file_has_md5(f, check)
-			continue
-
-for ioc in IOCS:
-	for f in IOCS[ioc]['files']:
-		if IOCS[ioc]['files'][f]:
+	if 'md5' in CHECKLIST[f]['modes']:
+		h = compute_md5_of_file(f)
+		md5s = build_dict_of_checks(CHECKLIST[f]['iocs'], 'md5')
+		for ioc in md5s:
+			if h == md5s[ioc]:
+				counter = store_match(f, ioc, counter)
+	if 'regex' in CHECKLIST[f]['modes']:
+		regexes = build_dict_of_checks(CHECKLIST[f]['iocs'], 'regex')
+		try:
+			fd = open(f)
+		except IOError:
 			if options.verbose:
-				print("POSITIVE MATCH FOUND: '%s' matches '%s'" %
-					  (f, ioc))
+				print("[ERROR] Can't open file '%s'" % f)
+		for line in fd:
+			for ioc in regexes:
+				if regexes[ioc].match(line):
+					counter = store_match(f, ioc, counter)
+
+print("%s results found." % (counter,))
+for ioc in RESULTS:
+	print("IOC '%s' present in '%s'" % (ioc, RESULTS[ioc]))
